1d6c90a348501854eac79965414293303b751fdb
Duda Sena <duda.sruiz@gmail.com>
2025-10-26 19:21:16 -0300

    page.js- import renomeado.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 "use client";
-import dynamic from "next/dynamic";
 import { BrowserRouter } from "react-router-dom";
+import loadDynamic from "next/dynamic"; 
 
-export const dynamic = "force-dynamic"; 
+export const dynamic = "force-dynamic"; // agora não conflita com o import acima
 
-const App = dynamic(() => import("./App.jsx"), { ssr: false });
+const App = loadDynamic(() => import("./App.jsx"), { ssr: false });
 
 export default function Page() {
   return (
